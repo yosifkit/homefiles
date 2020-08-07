0cd0845b3296a2edf8fb5bd5b2f9ba3a80bcaae8
Joe Ferguson <yosifkit@gmail.com>
2020-08-07 15:53:55 -0700

    Customize git-config; improve helper script
     - drop need for "tianon" upstream and "master" branch

--- rebase-yosifkit.sh
+++ rebase-yosifkit.sh
@@ -3,11 +3,6 @@ set -e
 
 thisDir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-if [ -e "$thisDir/resync-master.sh" ]; then
-	"$thisDir/resync-master.sh"
-fi
-
-git -C "$thisDir" checkout yosifkit
-git -C "$thisDir" rebase master
+git -C "$thisDir" pull --rebase 'https://github.com/tianon/home.git' master
 git -C "$thisDir" status
-#git -C "$thisDir" push -f
+echo git -C "$thisDir" push -f
--- resync-master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-thisDir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
-git -C "$thisDir" fetch --all -p
-git -C "$thisDir" checkout master
-git -C "$thisDir" pull tianon master
-git -C "$thisDir" push
