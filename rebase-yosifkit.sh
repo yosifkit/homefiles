39a65ef78ac9a7dce97925e871c9412615cd961d
Joe Ferguson <yosifkit@gmail.com>
2020-08-07 17:22:47 -0700

    Improve script to make git happy during rebase (and fix the config)

--- rebase-yosifkit.sh
+++ rebase-yosifkit.sh
@@ -3,6 +3,17 @@ set -e
 
 thisDir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-git -C "$thisDir" pull --rebase 'https://github.com/tianon/home.git' master
+# git getting confused when git-config files disappear
+rm -f "$thisDir/git-config.d/personal" # remove symlink
+
+git \
+	-C "$thisDir" \
+	-c user.name=foo \
+	-c user.email=bar@baz \
+	pull \
+	--rebase 'https://github.com/tianon/home.git' master
+
+ln -s yosifkit "$thisDir/git-config.d/personal"
+
 git -C "$thisDir" status
 echo git -C "$thisDir" push -f
